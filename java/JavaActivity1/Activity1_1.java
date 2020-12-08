8e1ea06d5ab3ede5eb6b57d0e511dc73d3890807
riskuma1 <48233654+riskuma1@users.noreply.github.com>
2020-12-08 16:03:39 +0530

    Add files via upload

---
+++ java/JavaActivity1/Activity1_1.java
@@ -0,0 +1,17 @@
+package JavaActivity1;
+
+public class Activity1_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Car kia = new Car();
+		kia.make = 2014;
+		kia.Color="Blue";
+		kia.transmission = "AMT";
+		
+		kia.displayCharacteristics();
+		kia.accelarate();
+		kia.brake();
+	}
+
+}
