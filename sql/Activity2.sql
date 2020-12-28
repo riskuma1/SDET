d244c74a1a21003bc02afbe1e23aa5258ee031ce
riskuma1 <48233654+riskuma1@users.noreply.github.com>
2020-12-28 09:35:47 +0530

    Add files via upload

---
+++ sql/Activity2.sql
@@ -0,0 +1,41 @@
+REM   Script: Activity 02
+REM   Activity 02
+
+CREATE TABLE salesman ( 
+	 
+    salesman_id int, 
+	 
+    salesman_name varchar2(32), 
+	 
+    salesman_city varchar2(32), 
+	 
+    commission int 
+	 
+);
+
+CREATE TABLE salesman ( 
+	 
+    salesman_id int, 
+	 
+    salesman_name varchar2(32), 
+	 
+    salesman_city varchar2(32), 
+	 
+    commission int 
+	 
+);
+
+DESCRIBE salesman
+
+
+INSERT INTO salesman VALUES(5002, 'Nail Knite', 'Paris', 13);
+
+INSERT ALL 
+    INTO salesman VALUES(5005, 'Pit Alex', 'London', 11) 
+    INTO salesman VALUES(5006, 'McLyon', 'Paris', 14) 
+    INTO salesman VALUES(5007, 'Paul Adam', 'Rome', 13) 
+    INTO salesman VALUES(5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM salesman;
+
---
+++ sql/Activity4.sql
@@ -0,0 +1,9 @@
+REM   Script: Activity 04
+REM   Activity 04
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100;
+
+SELECT * FROM salesman;
+
